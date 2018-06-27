8aece8865ab5fd4f900427afd92f6022528efffa
Akash Gajjar <akashgajjar8@gmail.com>
2018-06-27 13:02:36 +0530

    update boilerplate

--- readme.md
+++
@@ -1,6 +0,0 @@
-# React Bolierplate
-React boilerplate code to get you started in secondes.  
-
-_please note that the modules used in this project may be obsolete and from the ancient times, you should consider updating modules as you wish since some of the dependencies may not work_  
-
-Extracted from my project [Expensify](https://github.com/skywalker212/expensify)
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,109 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
-}
-
-module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
-
-  return {
-    entry: ['babel-polyfill', './src/app.js'],
-    output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      }]
-    },
-    plugins: [
-      CSSExtract,
-      new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-      })
-    ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true,
-      publicPath: '/dist/'
+var config = {
+  entry: {
+    bundle: APP_DIR + '/index.jsx'
+  },
+  devtool: 'source-map',
+  output: {
+    path: BUILD_DIR,
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: '.',
+    port: 3000,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/
     }
-  };
+  },
+  module : {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'eslint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      // font-awesome font loading inspired by https://gist.github.com/Turbo87/e8e941e68308d3b40ef6
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "url-loader?limit=10000&mimetype=application/font-woff",
+            options: {
+              name: "fonts/[hash].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[hash].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all"
+        }
+      }
+    }
+  }
 };
+
+module.exports = config;
